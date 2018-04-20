a2efdf914d587a2079a9606cca9fdb89af9e6d59
rthorup <ryan.thorup@gmail.com>
2018-04-20 15:18:24 -0500

    added attributes features

--- js/assignment_main.js
+++ js/assignment_main.js
@@ -1,7 +1,65 @@
 'use strict';
-// //declaring empty array to use later
+var start = new Date().getTime();
+console.log(start);
+// //declaring arrays to capture data
 let assignmentData = [];
+let skills = [];
+let keywords = [];
+let activity = [];
+let course = [];
 
+//pulling in json data to work with on page load.
+window.onload = function() {
+	axios.get('https://spencerdev.dept.ku.edu/RJT')
+		.then(function(response) {
+			//main data
+			assignmentData = response.data;
+			//breaking down list items programmatically
+			let tempSkillsArray = [];
+			let tempKeywordArray = [];
+			let tempCourseArray = [];
+			let tempActivityArray = [];
+			//breaking the strings into array items
+			assignmentData.forEach((item) => {
+				let skillsArray = item.Skills.split(',');
+				let keywordArray = item.Keywords.split(',');
+				let courseArray = item.CourseCodeAndNumber.split(',');
+				let activityArray = item.ActivityType.split(',');
+
+				skillsArray.forEach((item) => {
+					tempSkillsArray.push(item);
+				})
+				keywordArray.forEach((item) => {
+					tempKeywordArray.push(item);
+				})
+				courseArray.forEach((item) => {
+					tempCourseArray.push(item);
+				})
+				activityArray.forEach((item) => {
+					tempActivityArray.push(item);
+				})
+			})
+			skills = removeDuplicates(tempSkillsArray).sort();
+			keywords = removeDuplicates(tempKeywordArray).sort();
+			course = removeDuplicates(tempCourseArray).sort();
+			activity = removeDuplicates(tempActivityArray).sort();
+			console.log(new Date().getTime() - start);
+		})
+		.catch(function(error) {
+			console.log(error);
+			alert('Sorry, we could not find any assignments at this time')
+		});
+}
+//function to remove duplicate entries from data;
+function removeDuplicates(arr) {
+	let filteredList = []
+	for (let i = 0; i < arr.length; i++) {
+		if (filteredList.indexOf(arr[i]) == -1) {
+			filteredList.push(arr[i])
+		}
+	}
+	return filteredList;
+}
 //make a div to put in assignment data and then append it to the dom
 const initPage = function(item) {
 	let markup = createDiv(item);
@@ -18,18 +76,6 @@ function createDiv(item) {
 		<h5>Skills used: ${item.Skills}</h5>
 		<h5>Keywords: ${item.Keywords}</h5>
 	`
-}
-
-//pulling in json data to work with on page load.
-window.onload = function() {
-	axios.get('https://spencerdev.dept.ku.edu/RJT')
-		.then(function(response) {
-			assignmentData = response.data;
-		})
-		.catch(function(error) {
-			console.log(error);
-			alert('Sorry, we could not find any assignments at this time')
-		});
 }
 
 let newView = document.getElementById('submitCall');
