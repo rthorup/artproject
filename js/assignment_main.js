33889cad669069a0b9f6f9907c48bad40fe0b3a7
rthorup <ryan.thorup@gmail.com>
2018-05-15 15:21:45 -0500

    added department and level search

--- js/assignment_main.js
+++ js/assignment_main.js
@@ -7,26 +7,29 @@ let assignmentData = [];
 let skills = [];
 let keywords = [];
 let activity = [];
-let course = [];
+let department = [];
+let size = [];
+let level = [];
 
 //pulling in json data to work with on page load.
 window.onload = function() {
-	axios.get('https://spencerdev.dept.ku.edu/RJT')
+	axios.get('http://spencerdev.dept.ku.edu/RJT')
 		.then(function(response) {
 			//main data
 			assignmentData = response.data;
 			//breaking down list items programmatically
 			let tempSkillsArray = [];
 			let tempKeywordArray = [];
-			let tempCourseArray = [];
 			let tempActivityArray = [];
+			let tempDepartmentArray = [];
+			let tempLevelArray = [];
+			let classSize = [];
 			//breaking the strings into array items
 			assignmentData.forEach((item) => {
 				//checking for null variables in each object.
 				let flag = true;
 				for (var key in item) {
 					if (item[key] === null) {
-						console.log("this is null");
 						flag = false;
 						break;
 					}
@@ -38,8 +41,11 @@ window.onload = function() {
 					//go ahead and process everything you need to.
 					let skillsArray = item.Skills.split(',');
 					let keywordArray = item.Keywords.split(',');
-					let courseArray = item.CourseCodeAndNumber.split(',');
 					let activityArray = item.ActivityType.split(',');
+					let departmentArray = item.SchoolDepartment.split(',');
+					let levelArray = item.AcademicLevel.split(',');
+					classSize.push(item.ClassSize.trim());
+
 					//loading all array items into temp array and trimming the extra white space before some of the entries
 					skillsArray.forEach((item) => {
 						tempSkillsArray.push(item.trim());
@@ -47,42 +53,63 @@ window.onload = function() {
 					keywordArray.forEach((item) => {
 						tempKeywordArray.push(item.trim());
 					})
-					courseArray.forEach((item) => {
-						tempCourseArray.push(item.trim());
-					})
 					activityArray.forEach((item) => {
 						tempActivityArray.push(item.trim());
+					})
+					departmentArray.forEach((item) => {
+						tempDepartmentArray.push(item.trim());
+					})
+					levelArray.forEach((item) => {
+						tempLevelArray.push(item.trim());
 					})
 				}
 			})
 			//sort the resulting data alphabetically after removing duplicates.
 			skills = removeDuplicates(tempSkillsArray).sort();
 			keywords = removeDuplicates(tempKeywordArray).sort();
-			course = removeDuplicates(tempCourseArray).sort();
 			activity = removeDuplicates(tempActivityArray).sort();
+			department = removeDuplicates(tempDepartmentArray).sort();
+			level = removeDuplicates(tempLevelArray).sort();
+			size = removeDuplicates(classSize).sort();
 			console.log(new Date().getTime() - start);
 		}).then(() => {
 			//capturing arrays to insert into autocomplete search;
 			let searchSkillList = skills;
 			let searchKeywordList = keywords;
-			let searchCourseList = course;
 			let searchActivityList = activity;
+			let searchDepartmentList = department;
+			let searchLevelList = level;
+			let searchSizeList = size;
 			//attaching lists to each autocomplete search
 			//if you need more info, check the autocmplete.js file for code base
 			$('#autocompleteSkills').autocomplete({
 				lookup: searchSkillList,
+				minChars: 0,
 				onSelect: function(suggestion) {}
 			});
 			$('#autocompleteKeywords').autocomplete({
 				lookup: searchKeywordList,
-				onSelect: function(suggestion) {}
-			});
-			$('#autocompleteCourse').autocomplete({
-				lookup: searchCourseList,
+				minChars: 0,
 				onSelect: function(suggestion) {}
 			});
 			$('#autocompleteActivity').autocomplete({
 				lookup: searchActivityList,
+				minChars: 0,
+				onSelect: function(suggestion) {}
+			});
+			$('#autocompleteDepartment').autocomplete({
+				lookup: searchDepartmentList,
+				minChars: 0,
+				onSelect: function(suggestion) {}
+			});
+			$('#autocompleteLevel').autocomplete({
+				lookup: searchLevelList,
+				minChars: 0,
+				onSelect: function(suggestion) {}
+			});
+			$('#autocompleteSize').autocomplete({
+				lookup: searchSizeList,
+				minChars: 0,
 				onSelect: function(suggestion) {}
 			});
 		})
@@ -103,33 +130,38 @@ function removeDuplicates(arr) {
 	return filteredList;
 }
 //make a div to put in assignment data and then append it to the dom
+let post = document.getElementById('post');
 const initPage = function(item) {
 	let markup = createDiv(item);
 	let container = document.createElement("div");
 	container.classList.add("assignmentBox");
 	container.innerHTML = markup;
-	document.body.appendChild(container);
+	post.appendChild(container);
 }
 //function to template divs with assignment data
 function createDiv(item) {
 	return `
 		<a href='http://spencerdev.dept.ku.edu/assignment?value=${item.ExhibitionName}' target="_blank"><h1>${item.ExhibitionName}</h1></a>
 		<h4>${item.AssignmentDescription}</h4>
-		<h5>Skills used: ${item.Skills}</h5>
-		<h5>Keywords: ${item.Keywords}</h5>
+		<h5><strong>Activity Type:</strong> ${item.ActivityType}</h5>
+		<h5><strong>Keywords:</strong> ${item.Keywords}</h5>
+		<h5><strong>Department:</strong> ${item.SchoolDepartment}</h5>
+		<h5><strong>Academic Level:</strong> ${item.AcademicLevel}</h5>
 	`
 }
 
 let newView = document.getElementById('submitCall');
 //function set to get search criteria
 newView.addEventListener('click', function() {
-	//clearing previous results if any
-	resetPage();
 	//getting data from select fields
 	let skillType = document.getElementById('autocompleteSkills').value;
 	let keywordType = document.getElementById('autocompleteKeywords').value;
-	let courseType = document.getElementById('autocompleteCourse').value;
 	let activityType = document.getElementById('autocompleteActivity').value;
+	let departmentType = document.getElementById('autocompleteDepartment').value;
+	let levelType = document.getElementById('autocompleteLevel').value;
+	let sizeType = document.getElementById('autocompleteSize').value;
+	//clearing previous results if any
+	resetPage();
 	//declaring the array to put matching criteria
 	let activityDisplay = []
 	//looping through our data to check for matching items/accounting for any NULL values
@@ -140,13 +172,19 @@ newView.addEventListener('click', function() {
 		if (assignmentData[i].Skills === null) {
 			continue;
 		}
-		if (assignmentData[i].CourseCodeAndNumber === null) {
-			continue;
-		}
 		if (assignmentData[i].Keywords === null) {
 			continue;
 		}
-		if ((assignmentData[i].ActivityType.includes(activityType)) && (assignmentData[i].Skills.includes(skillType)) && (assignmentData[i].CourseCodeAndNumber.includes(courseType)) && (assignmentData[i].Keywords.includes(keywordType))) {
+		if (assignmentData[i].AcademicLevel === null) {
+			continue;
+		}
+		if (assignmentData[i].SchoolDepartment === null) {
+			continue;
+		}
+		if (assignmentData[i].ClassSize === null) {
+			continue;
+		}
+		if ((assignmentData[i].ActivityType.includes(activityType)) && (assignmentData[i].Skills.includes(skillType)) && (assignmentData[i].Keywords.includes(keywordType)) && (assignmentData[i].AcademicLevel.includes(levelType)) && (assignmentData[i].SchoolDepartment.includes(departmentType)) && (assignmentData[i].ClassSize.includes(sizeType))) {
 			activityDisplay.push(assignmentData[i]);
 		}
 	}
@@ -162,14 +200,16 @@ newView.addEventListener('click', function() {
 
 let resetSearch = document.getElementById('resetSearch');
 resetSearch.addEventListener('click', function() {
-	document.getElementById('autocompleteSkills').value = "";
-	document.getElementById('autocompleteKeywords').value = "";
-	document.getElementById('autocompleteCourse').value = "";
-	document.getElementById('autocompleteActivity').value = "";
 	resetPage();
 });
 
 function resetPage() {
+	document.getElementById('autocompleteSkills').value = "";
+	document.getElementById('autocompleteKeywords').value = "";
+	document.getElementById('autocompleteActivity').value = "";
+	document.getElementById('autocompleteDepartment').value = "";
+	document.getElementById('autocompleteLevel').value = "";
+	document.getElementById('autocompleteSize').value = "";
 	let divs = document.querySelectorAll('.assignmentBox');
 	for (let i = 0; i < divs.length; i++) {
 		divs[i].parentNode.removeChild(divs[i]);
